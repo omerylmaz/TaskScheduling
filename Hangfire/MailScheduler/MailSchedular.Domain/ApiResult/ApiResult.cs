beae60fb394f1fbf221341a13f96f0cca205398b
Ömer Yılmaz <omerisler20@gmail.com>
2023-05-08 22:44:24 +0300

    first commit

---
+++ Hangfire/MailScheduler/MailSchedular.Domain/ApiResult/ApiResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSchedular.Domain.ApiResult
+{
+    public class ApiResult<T>
+    {
+        public bool IsSuccess { get; set; }
+        public List<string> Messages { get; set; } = new();
+        public T Response { get; set; }
+
+        public ApiResult<T> SetResponse(T response, string? message = null)
+        {
+            Response = response;
+            IsSuccess = true;
+            if (message != null)
+            {
+                Messages.Add(message);
+            }
+
+            return this;
+        }
+
+        public ApiResult<T> SetError(string errorMessage)
+        {
+            IsSuccess = false;
+            Messages.Add(errorMessage);
+            return this;
+        }
+
+        public ApiResult<T> SetError(List<string> messages)
+        {
+            IsSuccess = false;
+            Messages.AddRange(messages);
+            return this;
+        }
+    }
+}
---
+++ Hangfire/MailScheduler/MailSchedular.Domain/Entities/MailMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSchedular.Domain.Entities
+{
+    public class MailMessage : BaseEntity
+    {
+        public string ToMail { get; set; }
+        public string Message { get; set; }
+    }
+}
